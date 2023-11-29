d411e7175e1cab679959a8780197a2fb434f8aea
Ignacionande98 <128426701+Ignacionande98@users.noreply.github.com>
2023-11-29 17:41:19 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,12 +1,13 @@
-const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
-const PUBLISH_PRODUCT_URL =
-  "https://japceibal.github.io/emercado-api/sell/publish.json";
-const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/";
-const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
-const PRODUCT_INFO_COMMENTS_URL =
-  "https://japceibal.github.io/emercado-api/products_comments/";
-const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
-const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
+const catID = localStorage.getItem("catID"); 
+const productID = localStorage.getItem("productID");
+
+const CATEGORIES_URL = "http://localhost:3000/cats"; 
+const PUBLISH_PRODUCT_URL = "http://localhost:3000/sell";
+const PRODUCTS_URL = `http://localhost:3000/cats/${catID}`; 
+const PRODUCT_INFO_URL = `http://localhost:3000/products/${productID}`; 
+const PRODUCT_INFO_COMMENTS_URL = `http://localhost:3000/products_comments/${productID}`; 
+const CART_INFO_URL = "http://localhost:3000/user_cart"; 
+const CART_BUY_URL = "http://localhost:3000/cart"; 
 const EXT_TYPE = ".json";
 
 let showSpinner = function () {
